16df6d2c0ec7d2520832f2b418d34fe27a8f0f71
Shota <gladius.vand@gmail.com>
2020-09-02 19:08:37 +0400

    Moved PhonesController to HomeController. Moved its views accordingly

--- Phones/Controllers/HomeController.cs
+++ Phones/Controllers/HomeController.cs
@@ -4,23 +4,159 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Phones.Data;
 using Phones.Models;
 
 namespace Phones.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly PhonesContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(PhonesContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        // GET: Phones
+        public async Task<IActionResult> Index()
         {
+            var phonesContext = _context.Phones.Include(p => p.Producer);
+            return View(await phonesContext.ToListAsync());
+        }
+
+        // GET: Phones/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var phone = await _context.Phones
+                .Include(p => p.Producer)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (phone == null)
+            {
+                return NotFound();
+            }
+
+            return View(phone);
+        }
+
+        // GET: Phones/Create
+        public IActionResult Create()
+        {
+            ViewData["ProducerId"] = new SelectList(_context.Producers, "Id", "Id");
             return View();
+        }
+
+        // POST: Phones/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ProducerId,Name,Size,Weight,ScreenSize,ScreenDepth,Processor,Storage,OperatingSystem,Price,ImgUrl,VidUrl")] Phone phone)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(phone);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProducerId"] = new SelectList(_context.Producers, "Id", "Id", phone.ProducerId);
+            return View(phone);
+        }
+
+        // GET: Phones/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var phone = await _context.Phones.FindAsync(id);
+            if (phone == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProducerId"] = new SelectList(_context.Producers, "Id", "Id", phone.ProducerId);
+            return View(phone);
+        }
+
+        // POST: Phones/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ProducerId,Name,Size,Weight,ScreenSize,ScreenDepth,Processor,Storage,OperatingSystem,Price,ImgUrl,VidUrl")] Phone phone)
+        {
+            if (id != phone.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(phone);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PhoneExists(phone.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProducerId"] = new SelectList(_context.Producers, "Id", "Id", phone.ProducerId);
+            return View(phone);
+        }
+
+        // GET: Phones/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var phone = await _context.Phones
+                .Include(p => p.Producer)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (phone == null)
+            {
+                return NotFound();
+            }
+
+            return View(phone);
+        }
+
+        // POST: Phones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var phone = await _context.Phones.FindAsync(id);
+            _context.Phones.Remove(phone);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PhoneExists(int id)
+        {
+            return _context.Phones.Any(e => e.Id == id);
         }
 
         public IActionResult Privacy()
