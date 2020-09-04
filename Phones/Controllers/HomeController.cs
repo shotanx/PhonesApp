0af827471ef09092991b8de69d4b4bbfe33085de
Shota <gladius.vand@gmail.com>
2020-09-04 22:51:26 +0400

    removed unnecessary usings

--- Phones/Controllers/HomeController.cs
+++ Phones/Controllers/HomeController.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using Phones.Data;
 using Phones.Models;
 
--- Phones/Data/PhonesContext.cs
+++ Phones/Data/PhonesContext.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Phones.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Phones.Data
 {
